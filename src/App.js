d3ea2e6a7b92636d5ac7c457f05be3f780002832
Nakakari <sarinandhika@gmail.com>
2022-10-22 16:30:54 +0700

    helo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Child = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Heloo, I am a child</h2>
+  );
+}
+
+const App = () => {
+  console.log('Heloo');
+
+  return (
+    <div>
+      <h1>My New component</h1>
+      <hr />
+      <input />
+      <Child />
     </div>
   );
 }
